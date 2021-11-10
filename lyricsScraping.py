96b707caaea09be688f3e09ea4c8e476f27fefda
Ayah Kamal <ayahk1999@gmail.com>
2021-11-10 19:38:25 +0300

    changes to syntax

--- lyricsScraping.py
+++ lyricsScraping.py
@@ -1,31 +1,78 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, random, time
+import shadow_useragent
+from random import randrange
 from lyricsgenius import Genius
+from collections import OrderedDict
 
 uri = "http://localhost:5000/"
 genius_key = "9rXoiyhZUYHiTC_-KU7_gTP5nb8okqTxL-O2g0HhFBjMS_8sAUkqR1vHexTO8LVe"
+
+from bs4 import BeautifulSoup
+import random
+import requests
+
+USER_AGENT_SCRAPER_BASE_URL = 'http://www.useragentstring.com/pages/useragentstring.php?name='
+
+POPULAR_BROWSERS = ['Chrome', 'Firefox', 'Mozilla', 'Safari', 'Opera', 'Opera Mini', 'Edge', 'Internet Explorer']
+
+def get_user_agent_strings_for_this_browser(browser):
+    """
+    Get the latest User-Agent strings of the given Browser
+    :param browser: string of given Browser
+    :return: list of User agents of the given Browser
+    """
+
+    url = USER_AGENT_SCRAPER_BASE_URL + browser
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    user_agent_links = soup.find('div', {'id': 'liste'}).findAll('a')[:20]
+
+    return [str(user_agent.text) for user_agent in user_agent_links]
+
+
+def get_user_agents():
+    """
+    Gather a list of some active User-Agent strings from
+    http://www.useragentstring.com of some of the Popular Browsers
+    :return: list of User-Agent strings
+    """
+
+    user_agents = []
+    for browser in POPULAR_BROWSERS:
+        user_agents.extend(get_user_agent_strings_for_this_browser(browser))
+    return user_agents[3:] # Remove the first 3 Google Header texts from Chrome's user agents
+
+proxy_user_agents = get_user_agents()
 
 #function to scrape lyrics from genius
 def scrape_lyrics(artistname, songname):
     genius = Genius(genius_key)
     #genius.verbose = False
     genius.remove_section_headers = True    
-   
+
+    # To randomly select an User-Agent from the collected user-agent strings
+    random_user_agent = random.choice(proxy_user_agents)
+    headers = {"User-Agent": random_user_agent}
+  
+    #Make the request
     artistname2 = str(artistname.replace(' ','-')) if ' ' in artistname else str(artistname)
     artistname2 = str(artistname2.replace("'",''))
     artistname2 = str(artistname2.replace(".",''))
-    print(artistname2)
+    #print(artistname2)
     songname2 = str(songname.replace(' ','-')) if ' ' in songname else str(songname)
     songname2 = str(songname2.replace("'",'')) 
     songname2 = str(songname2.replace("(",'')) 
     songname2 = str(songname2.replace(")",'')) 
     songname2 = str(songname2.replace("!",'')) 
-    print(songname2)
-    page = requests.get('https://genius.com/'+ artistname2 + '-' + songname2 + '-' + 'lyrics')
+    #print(songname2)
+    page = requests.get('https://genius.com/'+ artistname2 + '-' + songname2 + '-' + 'lyrics', headers)
     html = BeautifulSoup(page.text, 'html.parser')
 
     lyrics1 = html.find("div", class_="lyrics")
+    time.sleep(randrange(5,15))
     lyrics2 = html.find("div", class_="Lyrics__Container-sc-1ynbvzw-2 jgQsqn")
+    time.sleep(randrange(5,15))
 
     if lyrics1:
         lyrics = lyrics1.get_text()
@@ -35,13 +82,13 @@ def scrape_lyrics(artistname, songname):
       #  lyrics = re.sub('[Verse]','',lyrics) # Remove [Verse] and [Bridge] stuff 
     elif lyrics1 == lyrics2 == None:
         artist = genius.search_artist(artistname,max_songs=1)
+        time.sleep(randrange(5,20))
         song = artist.song(songname)
         if song == None:
             return None
         else:
             return song.lyrics
     return lyrics
-
 
 #function to attach lyrics onto data frame
 #artist_name should be inserted as a string
@@ -51,6 +98,7 @@ def lyrics_onto_frame(df1, artist_name):
             test = scrape_lyrics(artist_name[i], x)
             df1.loc[i, 'lyrics'] = test
         return df1
+
 
 
     
---
+++ spotifyCreatedData.py
@@ -0,0 +1,138 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+from lyricsScraping import *
+
+clientID = "6b3d7d7b52324366a650f2d43388ffe2"
+clientSecret = "c2666658cce64c1bae9c371eccca0a9c"
+
+client_credentials_manager = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+# Spotify generated playlist according to certain moods
+sad_songs_playlist = "37i9dQZF1DX7qK8ma5wgG1"
+sad_songs_playlist_1 = "37i9dQZF1DWX83CujKHHOn"
+sad_songs_playlist_2 = "38BAFacbwA5kGf7iqcW7Wj"
+happy_songs_playlist = "37i9dQZF1DXdPec7aLTmlC"
+happy_songs_playlist_1 = "37i9dQZF1DX9XIFQuFvzM4"
+happy_songs_playlist_2 = "37i9dQZF1DX3rxVfibe1L0"
+energy_songs_playlist = "37i9dQZF1DX0vHZ8elq0UK"
+energy_songs_playlist_1 = "37i9dQZF1DX8hY56Fq3fM0"
+calm_songs_playlist = "37i9dQZF1DWWpO97CaFM3p"
+calm_songs_playlist_1 = "37i9dQZF1DXaXDsfv6nvZ5"
+calm_songs_playlist_2 = "11q1sTpWABVhGmIBcfDLtt"
+angry_songs_playlist = "6ft4ijUITtTeVC0dUCDdvH"
+angry_songs_playlist_1 ="3ZzI0mlze2kT37u9lQwUcO"
+angry_songs_playlist_2 = "0s5O323LN8dpV81h33OoZQ"
+
+
+def call_playlist(creator, playlist_id):
+    
+    playlist_features_list = ["artist","album","track", "track_id",'danceability',
+ 'energy',
+ 'key',
+ 'loudness',
+ 'speechiness',
+ 'acousticness',
+ 'instrumentalness',
+ 'liveness',
+ 'valence',
+ 'tempo', "duration_ms","time_signature"]
+
+    playlist_df = pd.DataFrame(columns = playlist_features_list)
+
+    playlist = sp.user_playlist_tracks(creator, playlist_id)["items"]
+    for track in playlist:
+        # Create empty dict
+        playlist_features = {}
+        # Get metadata
+        playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
+        playlist_features["album"] = track["track"]["album"]["name"]
+        playlist_features["track"] = track["track"]["name"]
+        playlist_features["duration_ms"] = track['track']['duration_ms']
+        playlist_features["track_id"] = track["track"]["id"]
+        
+        # Get audio features
+        audio_features = sp.audio_features(playlist_features["track_id"])[0]
+        for feature in playlist_features_list[4:]:
+            playlist_features[feature] = audio_features[feature]
+        
+        # Concat the dfs
+        track_df = pd.DataFrame(playlist_features, index = [0])
+        playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
+        
+    return playlist_df
+
+sad_songs_df = call_playlist("spotify",sad_songs_playlist)
+happy_songs_df = call_playlist("spotify",happy_songs_playlist)
+energy_songs_df = call_playlist("spotify",energy_songs_playlist)
+calm_songs_df = call_playlist("spotify",calm_songs_playlist)
+
+sad_songs_df1 = call_playlist("spotify",sad_songs_playlist_1)
+happy_songs_df1 = call_playlist("spotify",happy_songs_playlist_1)
+happy_songs_df2 = call_playlist("spotify",happy_songs_playlist_2)
+energy_songs_df1 = call_playlist("spotify",energy_songs_playlist_1)
+calm_songs_df1 = call_playlist("spotify",calm_songs_playlist_1)
+
+calm_songs_df2 = call_playlist("spotify",calm_songs_playlist_2)
+angry_songs_df = call_playlist("spotify",angry_songs_playlist)
+angry_songs_df1 = call_playlist("spotify",angry_songs_playlist_1)
+angry_songs_df2 = call_playlist("spotify",angry_songs_playlist_2)
+sad_songs_df2 = call_playlist("spotify",sad_songs_playlist_2)
+
+sad_songs_df['mood'] = 'Sad'
+sad_songs_df1['mood'] = 'Sad'
+sad_songs_df2['mood'] = 'Sad'
+happy_songs_df['mood'] = 'Happy'
+happy_songs_df1['mood'] = 'Happy'
+happy_songs_df2['mood'] = 'Happy'
+energy_songs_df['mood'] = 'Energy'
+energy_songs_df1['mood'] = 'Energy'
+calm_songs_df['mood'] = 'Calm'
+calm_songs_df1['mood'] = 'Calm'
+calm_songs_df2['mood'] = 'Calm'
+angry_songs_df['mood'] = 'Angry'
+angry_songs_df1['mood'] = 'Angry'
+angry_songs_df2['mood'] = 'Angry'
+
+# exporting dataframes as csv to scrape lyrics
+# worked in smaller batches to avoid getting blocked
+'''
+sad_songs_df.to_csv(r".\sad_mood_dataset.csv", index = False)
+sad_songs_df1.to_csv(r".\sad_mood_1_dataset.csv", index = False)
+sad_songs_df2.to_csv(r".\sad_mood_2_dataset.csv", index = False)
+
+happy_songs_df.to_csv(r".\happy_mood_dataset.csv", index = False)
+happy_songs_df1.to_csv(r".\happy_mood_1_dataset.csv", index = False)
+happy_songs_df2.to_csv(r".\happy_mood_2_dataset.csv", index = False)
+
+energy_songs_df.to_csv(r".\energy_mood_dataset.csv", index = False)
+energy_songs_df1.to_csv(r".\energy_mood_1_dataset.csv", index = False)
+
+calm_songs_df.to_csv(r".\calm_mood_dataset.csv", index = False)
+calm_songs_df1.to_csv(r".\calm_mood_1_dataset.csv", index = False)
+calm_songs_df2.to_csv(r".\calm_mood_2_dataset.csv", index = False)
+
+angry_songs_df.to_csv(r".\angry_mood_dataset.csv", index = False)
+angry_songs_df1.to_csv(r".\angry_mood_1_dataset.csv", index = False)
+angry_songs_df2.to_csv(r".\angry_mood_1_dataset.csv", index = False)
+'''
+
+sad_df = pd.concat([sad_songs_df, sad_songs_df1, sad_songs_df2])
+happy_df = pd.concat([happy_songs_df, happy_songs_df1, happy_songs_df2])
+energy_df = pd.concat([energy_songs_df, energy_songs_df1])
+calm_df = pd.concat([calm_songs_df, calm_songs_df1, calm_songs_df2])
+angry_df = pd.concat([angry_songs_df, angry_songs_df1, angry_songs_df2])
+
+def concatFrames(df1, df2, df3, df4, df5):
+    df = pd.concat([df1,df2,df3,df4,df5], 
+    ignore_index= True)
+    return df
+
+mood_data=concatFrames(sad_df, happy_df, energy_df, calm_df, angry_df)
+
+mood_data_df = mood_data.sort_values(by = 'track')
+mood_data_df = mood_data_df.reset_index(drop = True)
+
+mood_data_df.to_csv(r".\spotify_mood_dataset.csv", index = False)
+
