e9d4d903ea400e5628e9345aa36e2da0fbe6b2e6
Ayah Kamal <68741119+ayah-kamal@users.noreply.github.com>
2021-11-15 02:50:57 +0300

    Delete secrets.py

--- secrets.py
+++
@@ -1,7 +0,0 @@
-clientID = "6b3d7d7b52324366a650f2d43388ffe2"
-clientSecret = "c2666658cce64c1bae9c371eccca0a9c"
-
-genius_key = "9rXoiyhZUYHiTC_-KU7_gTP5nb8okqTxL-O2g0HhFBjMS_8sAUkqR1vHexTO8LVe"
-
-API_KEY = "AIzaSyAXInxjrBdYzXqHG0ri5rnGo-iu6WxZTFQ"
-GCS_ENGINE_ID = "d279e9fd8ba7ec0cb"
